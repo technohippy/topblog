e6a93026fd7f1b3faf8d786fe6eaedc879446bb7
andyjpn <andyjpn@72eec153-8952-0410-b136-593b7c5a8c50>
2008-07-22 17:41:50 +0000

    use protocalendar.js to edit the date property of the Entry model

--- trunk/topblog/blog/forms.py
+++ trunk/topblog/blog/forms.py
@@ -1,6 +1,8 @@
 from django import newforms as forms
 from google.appengine.ext.db import djangoforms
 import models
+import time
+import widgets
 
 class BlogForm(djangoforms.ModelForm):
 	class Meta:
@@ -10,4 +12,6 @@ class BlogForm(djangoforms.ModelForm):
 class EntryForm(djangoforms.ModelForm):
   class Meta:
     model = models.Entry
-    exclude = ['owner', 'created_on', 'tags', 'index']
+    #exclude = ['owner', 'created_on', 'tags', 'index']
+    exclude = ['owner', 'date', 'created_on', 'tags', 'index']
+  date = forms.DateField(widget=widgets.CalendarWidget, initial=time.strftime('%Y-%m-%d'))
